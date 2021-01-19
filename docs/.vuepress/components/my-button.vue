9c1b217834f356f0da1916ce3545dd585db3e2fa
a961309494 <961309494@qq.com>
2021-01-19 12:01:01 +0800

    20200119

---
+++ docs/.vuepress/components/my-button.vue
@@ -0,0 +1,18 @@
+<template>
+    <button @click="handleClick">今天天气可真好呀！</button>
+</template>
+
+<script>
+export default {
+    name: "my-button",
+    methods:{
+        handleClick(){
+          console.log("click!")
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
